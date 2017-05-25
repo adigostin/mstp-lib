3cfac2efc0c6a90a4a3389b05230254d172c7634
adigostin <adigostin@gmail.com>
2017-05-25 08:46:22 +0300

    Code cleanup.

--- Simulator/BridgePropsWindow.cpp
+++ Simulator/BridgePropsWindow.cpp
@@ -362,35 +362,6 @@ private:
 	void ValidateAndSetBridgeAddress (const wchar_t* str)
 	{
 		auto newAddress = ConvertStringToBridgeAddress(str);
-
-		struct SetBridgeAddressAction : public EditAction
-		{
-			vector<pair<Bridge*, STP_BRIDGE_ADDRESS>> _bridgesAndOldAddresses;
-			STP_BRIDGE_ADDRESS _newAddress;
-
-			SetBridgeAddressAction (const vector<Bridge*>& bridges, STP_BRIDGE_ADDRESS newAddress)
-			{
-				std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldAddresses),
-								[](Bridge* b) { return make_pair(b, *STP_GetBridgeAddress(b->GetStpBridge())); });
-				_newAddress = newAddress;
-			}
-
-			virtual void Redo() override final
-			{
-				auto timestamp = GetTimestampMilliseconds();
-				for (auto& p : _bridgesAndOldAddresses)
-					STP_SetBridgeAddress (p.first->GetStpBridge(), _newAddress.bytes, timestamp);
-			}
-			virtual void Undo() override final
-			{
-				auto timestamp = GetTimestampMilliseconds();
-				for (auto& p : _bridgesAndOldAddresses)
-					STP_SetBridgeAddress (p.first->GetStpBridge(), p.second.bytes, timestamp);
-			}
-
-			virtual std::string GetName() const override final { return "Set Bridge Address"; }
-		};
-
 		auto action = unique_ptr<EditAction>(new SetBridgeAddressAction(_bridges, newAddress));
 		_actionList->PerformAndAddUserAction (move(action));
 	}
@@ -460,35 +431,6 @@ private:
 
 		if (any_of (_bridges.begin(), _bridges.end(), [newVersion](Bridge* b) { return STP_GetStpVersion(b->GetStpBridge()) != newVersion; }))
 		{
-			struct ChangeStpVersionAction : public EditAction
-			{
-				vector<pair<Bridge*, STP_VERSION>> _bridgesAndOldVersions;
-				STP_VERSION _newVersion;
-
-				ChangeStpVersionAction (const vector<Bridge*>& bridges, STP_VERSION newVersion)
-				{
-					std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldVersions),
-									[](Bridge* b) { return make_pair(b, STP_GetStpVersion(b->GetStpBridge())); });
-					_newVersion = newVersion;
-				}
-
-				virtual void Redo() override final
-				{
-					auto timestamp = GetTimestampMilliseconds();
-					for (auto& p : _bridgesAndOldVersions)
-						STP_SetStpVersion (p.first->GetStpBridge(), _newVersion, timestamp);
-				}
-
-				virtual void Undo() override final
-				{
-					auto timestamp = GetTimestampMilliseconds();
-					for (auto& p : _bridgesAndOldVersions)
-						STP_SetStpVersion (p.first->GetStpBridge(), p.second, timestamp);
-				}
-
-				virtual std::string GetName() const override final { return "Change STP version"; }
-			};
-
 			auto action = unique_ptr<EditAction>(new ChangeStpVersionAction(_bridges, newVersion));
 			_actionList->PerformAndAddUserAction (move(action));
 		}
@@ -643,38 +585,14 @@ private:
 
 			ascii.push_back((char) *p);
 		}
+		ascii.resize(32);
 
-		struct SetMstConfigNameAction : public EditAction
+		auto same = [&ascii](Bridge* b) { return memcmp (ascii.data(), STP_GetMstConfigId(b->GetStpBridge())->ConfigurationName, 32) == 0; };
+		if (any_of (_bridges.begin(), _bridges.end(), [&same](Bridge* b) { return !same(b); }))
 		{
-			vector<pair<Bridge*, string>> _bridgesAndOldNames;
-			string _newName;
-
-			SetMstConfigNameAction (const vector<Bridge*>& bridges, string&& newName)
-			{
-				std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldNames),
-								[](Bridge* b) { return make_pair(b, string(STP_GetMstConfigId(b->GetStpBridge())->ConfigurationName, 32)); });
-				_newName = move(newName);
-			}
-
-			virtual void Redo() override final
-			{
-				auto timestamp = GetTimestampMilliseconds();
-				for (auto& p : _bridgesAndOldNames)
-					STP_SetMstConfigName (p.first->GetStpBridge(), _newName.c_str(), timestamp);
-			}
-
-			virtual void Undo() override final
-			{
-				auto timestamp = GetTimestampMilliseconds();
-				for (auto& p : _bridgesAndOldNames)
-					STP_SetMstConfigName (p.first->GetStpBridge(), p.second.c_str(), timestamp);
-			}
-
-			virtual string GetName() const override final { return "Set MST Config Name"; }
-		};
-
-		auto action = unique_ptr<EditAction>(new SetMstConfigNameAction(_bridges, move(ascii)));
-		_actionList->PerformAndAddUserAction(move(action));
+			auto action = unique_ptr<EditAction>(new SetMstConfigNameAction(_bridges, move(ascii)));
+			_actionList->PerformAndAddUserAction(move(action));
+		}
 	}
 	#pragma endregion
 
@@ -797,42 +715,6 @@ private:
 
 		if (any_of(_bridges.begin(), _bridges.end(), [=](Bridge* b) { return getPrio(b, vlan) != newPrio; }))
 		{
-			struct ChangeBridgePrioAction : EditAction
-			{
-				vector<pair<Bridge*, unsigned short>> _bridgesAndOldPrios;
-				unsigned short _newPrio;
-				unsigned int _vlan;
-
-				ChangeBridgePrioAction(const vector<Bridge*> bridges, unsigned short newPrio, unsigned int vlan)
-				{
-					std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldPrios), [=](Bridge* b) { return make_pair(b, getPrio(b, vlan)); });
-					_newPrio = newPrio;
-					_vlan = vlan;
-				}
-
-				virtual void Redo() override final
-				{
-					auto timestamp = GetTimestampMilliseconds();
-					for (auto& p : _bridgesAndOldPrios)
-					{
-						auto treeIndex = STP_GetTreeIndexFromVlanNumber (p.first->GetStpBridge(), _vlan);
-						STP_SetBridgePriority (p.first->GetStpBridge(), treeIndex, _newPrio, timestamp);
-					}
-				}
-
-				virtual void Undo() override final
-				{
-					auto timestamp = GetTimestampMilliseconds();
-					for (auto& p : _bridgesAndOldPrios)
-					{
-						auto treeIndex = STP_GetTreeIndexFromVlanNumber (p.first->GetStpBridge(), _vlan);
-						STP_SetBridgePriority (p.first->GetStpBridge(), treeIndex, p.second, timestamp);
-					}
-				}
-
-				virtual std::string GetName() const override final { return "Change bridge priority"; }
-			};
-
 			auto action = unique_ptr<EditAction>(new ChangeBridgePrioAction(_bridges, newPrio, vlan));
 			_actionList->PerformAndAddUserAction (move(action));
 		}
--- Simulator/EditActions.h
+++ Simulator/EditActions.h
@@ -86,3 +86,126 @@ struct EnableDisableStpAction : EditAction
 	virtual std::string GetName() const override final { return std::string (_newEnable ? "Enable STP" : "Disable STP"); }
 };
 
+struct SetBridgeAddressAction : public EditAction
+{
+	std::vector<std::pair<Bridge*, STP_BRIDGE_ADDRESS>> _bridgesAndOldAddresses;
+	STP_BRIDGE_ADDRESS _newAddress;
+
+	SetBridgeAddressAction (const std::vector<Bridge*>& bridges, STP_BRIDGE_ADDRESS newAddress)
+	{
+		std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldAddresses),
+						[](Bridge* b) { return std::make_pair(b, *STP_GetBridgeAddress(b->GetStpBridge())); });
+		_newAddress = newAddress;
+	}
+
+	virtual void Redo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldAddresses)
+			STP_SetBridgeAddress (p.first->GetStpBridge(), _newAddress.bytes, timestamp);
+	}
+	virtual void Undo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldAddresses)
+			STP_SetBridgeAddress (p.first->GetStpBridge(), p.second.bytes, timestamp);
+	}
+
+	virtual std::string GetName() const override final { return "Set Bridge Address"; }
+};
+
+struct SetMstConfigNameAction : public EditAction
+{
+	std::vector<std::pair<Bridge*, std::string>> _bridgesAndOldNames;
+	std::string _newName;
+
+	SetMstConfigNameAction (const std::vector<Bridge*>& bridges, std::string&& newName)
+	{
+		std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldNames),
+						[](Bridge* b) { return std::make_pair(b, std::string(STP_GetMstConfigId(b->GetStpBridge())->ConfigurationName, 32)); });
+		_newName = move(newName);
+	}
+
+	virtual void Redo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldNames)
+			STP_SetMstConfigName (p.first->GetStpBridge(), _newName.c_str(), timestamp);
+	}
+
+	virtual void Undo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldNames)
+			STP_SetMstConfigName (p.first->GetStpBridge(), p.second.c_str(), timestamp);
+	}
+
+	virtual std::string GetName() const override final { return "Set MST Config Name"; }
+};
+
+struct ChangeStpVersionAction : public EditAction
+{
+	std::vector<std::pair<Bridge*, STP_VERSION>> _bridgesAndOldVersions;
+	STP_VERSION _newVersion;
+
+	ChangeStpVersionAction (const std::vector<Bridge*>& bridges, STP_VERSION newVersion)
+	{
+		std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldVersions),
+						[](Bridge* b) { return std::make_pair(b, STP_GetStpVersion(b->GetStpBridge())); });
+		_newVersion = newVersion;
+	}
+
+	virtual void Redo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldVersions)
+			STP_SetStpVersion (p.first->GetStpBridge(), _newVersion, timestamp);
+	}
+
+	virtual void Undo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldVersions)
+			STP_SetStpVersion (p.first->GetStpBridge(), p.second, timestamp);
+	}
+
+	virtual std::string GetName() const override final { return "Change STP version"; }
+};
+
+struct ChangeBridgePrioAction : EditAction
+{
+	std::vector<std::pair<Bridge*, unsigned short>> _bridgesAndOldPrios;
+	unsigned short _newPrio;
+	unsigned int _vlan;
+
+	ChangeBridgePrioAction(const std::vector<Bridge*> bridges, unsigned short newPrio, unsigned int vlan)
+	{
+		auto getPrio = [](Bridge* b, unsigned int vlan) { return STP_GetBridgePriority(b->GetStpBridge(), STP_GetTreeIndexFromVlanNumber(b->GetStpBridge(), vlan)); };
+		std::transform (bridges.begin(), bridges.end(), back_inserter(_bridgesAndOldPrios), [=](Bridge* b) { return std::make_pair(b, getPrio(b, vlan)); });
+		_newPrio = newPrio;
+		_vlan = vlan;
+	}
+
+	virtual void Redo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldPrios)
+		{
+			auto treeIndex = STP_GetTreeIndexFromVlanNumber (p.first->GetStpBridge(), _vlan);
+			STP_SetBridgePriority (p.first->GetStpBridge(), treeIndex, _newPrio, timestamp);
+		}
+	}
+
+	virtual void Undo() override final
+	{
+		auto timestamp = GetTimestampMilliseconds();
+		for (auto& p : _bridgesAndOldPrios)
+		{
+			auto treeIndex = STP_GetTreeIndexFromVlanNumber (p.first->GetStpBridge(), _vlan);
+			STP_SetBridgePriority (p.first->GetStpBridge(), treeIndex, p.second, timestamp);
+		}
+	}
+
+	virtual std::string GetName() const override final { return "Change bridge priority"; }
+};
+
