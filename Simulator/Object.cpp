c8b01afd5661246d5513ccc5316d189af0414c03
adigostin <adigostin@gmail.com>
2017-06-08 11:34:21 +0300

    WIP

--- Simulator/Object.cpp
+++ Simulator/Object.cpp
@@ -2,10 +2,6 @@
 #include "pch.h"
 #include "Object.h"
 
-EnumProperty::EnumProperty (const wchar_t* name, Getter getter, Setter setter, const NVP* nameValuePairs)
-	: TypedProperty(name, getter, setter), _nameValuePairs(nameValuePairs)
-{ }
-
 std::wstring EnumProperty::to_wstring (const Object* obj, unsigned int vlanNumber) const
 {
 	auto value = _getter(obj, vlanNumber);
