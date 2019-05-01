86b213ebd6741b178fe72d971aac8ec5823ea394
adigostin <adigostin@gmail.com>
2019-05-01 10:52:08 +0300

    Added an empty test project to the VS solution.

---
+++ tests/bridge_tests.cpp
@@ -0,0 +1,21 @@
+
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "bridge.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests
+{
+	TEST_CLASS(tests)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			uint32_t port_count = 4;
+			uint32_t msti_count = 0;
+			mac_address address = { 0x10, 0x20, 0x30, 0x40, 0x50, 0x60 };
+			auto b = std::make_unique<bridge>(port_count, msti_count, address);
+		}
+	};
+}
