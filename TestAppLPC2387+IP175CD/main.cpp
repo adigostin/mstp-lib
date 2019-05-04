c68b0d49d489aa750ef0aaadad4e3a672dc29ac9
adigostin <adigostin@gmail.com>
2019-05-04 13:33:41 +0300

    Updated one of the sample embedded apps.

--- TestAppLPC2387+IP175CD/main.cpp
+++ TestAppLPC2387+IP175CD/main.cpp
@@ -20,7 +20,8 @@
 #define PORT_RMII		2
 
 extern STP_CALLBACKS const stp_callbacks;
-static STP_BRIDGE* bridge;
+extern const serial_command commands[];
+STP_BRIDGE* bridge = NULL;
 unsigned short chip_id;
 
 // ============================================================================
@@ -106,69 +107,6 @@ static void InitClock ()
 	PLLFEED = 0xAA;
 	PLLFEED = 0x55;
 }
-
-static void process_log_command (const char* params)
-{
-	if (*params == 0)
-	{
-		printf ("STP logging is currently %s.\r\n", STP_IsLoggingEnabled(bridge) ? "enabled" : "disabled");
-		return;
-	}
-
-	if (strcasecmp(params, "on") == 0)
-	{
-		STP_EnableLogging (bridge, true);
-		printf ("STP logging is now %s.\r\n", "enabled");
-	}
-	else if (strcasecmp(params, "off") == 0)
-	{
-		STP_EnableLogging (bridge, false);
-		printf ("STP logging is now %s.\r\n", "disabled");
-	}
-	else
-		printf ("Wrong params.\r\n");
-}
-
-static void process_stp_command (const char* params)
-{
-	if (*params == 0)
-	{
-		printf ("STP is currently %s.\r\n", STP_IsBridgeStarted(bridge) ? "enabled" : "disabled");
-		return;
-	}
-
-	if (strcasecmp(params, "on") == 0)
-	{
-		if (STP_IsBridgeStarted(bridge))
-			printf ("STP is already %s.\r\n", "enabled");
-		else
-		{
-			printf ("Enabling STP...\r\n");
-			STP_StartBridge(bridge, true);
-			printf ("STP is now %s.\r\n", "enabled");
-		}
-	}
-	else if (strcasecmp(params, "off") == 0)
-	{
-		if (!STP_IsBridgeStarted(bridge))
-			printf ("STP is already %s.\r\n", "disabled");
-		else
-		{
-			printf ("Disabling STP...\r\n");
-			STP_StopBridge(bridge, true);
-			printf ("STP is now %s.\r\n", "disabled");
-		}
-	}
-	else
-		printf ("Wrong params.\r\n");
-
-}
-
-static const serial_command commands[] = {
-	{ "log", "log [on|off] - Enables or disables STP logging.", &process_log_command },
-	{ "stp", "stp [on|off] - Enables or disables the protocol.", &process_stp_command },
-	{ 0, 0, 0 }
-};
 
 // ============================================================================
 
