c7ecd6294f7e117fc196147112ce50f78979551d
adigostin <adigostin@gmail.com>
2019-05-11 13:28:13 +0300

    Added CMake project for testing compilation of the library with the GnuARM that comes with Visual Studio (currently 6.3.1)

---
+++ build_test_gcc_arm_6.x/build_test_gcc_arm_6.x.cpp
@@ -0,0 +1,12 @@
+﻿// build_test_gcc_arm_6.x.cpp : Defines the entry point for the application.
+//
+
+#include "build_test_gcc_arm_6.x.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
