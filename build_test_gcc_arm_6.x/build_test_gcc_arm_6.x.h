8f9e5435e36a4bbfcdfbfd972570ee6056faea0e
adigostin <adigostin@gmail.com>
2020-02-27 23:19:42 +0200

    Fixed #25.

--- build_test_gcc_arm_6.x/build_test_gcc_arm_6.x.h
+++
@@ -1,8 +0,0 @@
-﻿// build_test_gcc_arm_6.x.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
